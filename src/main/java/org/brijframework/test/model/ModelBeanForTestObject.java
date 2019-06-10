fe8350d3497dcb78cfc0b2b52f19ebf675a5213a
ram kishor <ramkishor0509@gmail.com>
2019-06-10 20:47:22 +0530

    ok

--- src/main/java/org/brijframework/test/model/ModelBeanForTestObject.java
+++ src/main/java/org/brijframework/test/model/ModelBeanForTestObject.java
@@ -8,12 +8,14 @@ public class ModelBeanForTestObject {
 
 		Employee employee=new Employee();
 		//employee.setProperty("address", new Address());
-		employee.setProperty("id", "01010");
+		/*employee.setProperty("id", "01010");
 		employee.setProperty("name", "Ram");
 		employee.setProperty("address.line", "Noida1");
 		//employee.setProperty("addresseMap.address010.city", new City());
 		employee.setProperty("address.city.name", "Ok1");
 		employee.printObject();
+		*/
+		System.out.println(employee.typeOfProperty("address"));
 	}
 
 }
