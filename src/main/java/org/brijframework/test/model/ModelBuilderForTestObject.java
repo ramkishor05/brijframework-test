b6ab8e14153dad88a4b7cbdd5ccf43858d9a4dd5
workmy2018 <workmy2018@gmail.com>
2019-06-12 10:38:19 +0530

    ok

--- src/main/java/org/brijframework/test/model/ModelBuilderForTestObject.java
+++ src/main/java/org/brijframework/test/model/ModelBuilderForTestObject.java
@@ -2,17 +2,16 @@ package org.brijframework.test.model;
 
 import java.io.IOException;
 
+import org.brijframework.beans.Employee;
 import org.brijframework.model.builder.ModelBuilder;
 
 public class ModelBuilderForTestObject {
 
 	public static void main(String[] args) throws IOException {
 		
-		ModelBuilder builder=ModelBuilder.getBuilder("Employee");
-		builder.setProperty("name", "ram");
-		builder.setProperty("rollNo", "10");
-		builder.setProperty("address.line", "noida");
-		builder.setProperty("address.city.name", "Gr Noida");
+		ModelBuilder builder=ModelBuilder.getBuilder(Employee.class);
+		builder.setProperties("name~rollNo~address.line", "ram","10","noida");
+		System.out.println(builder.getProperties());
 		builder.printObject();
 	}
 }
