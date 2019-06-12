a7051447abbfefe1f3b45665a2bcf8f873d54205
ram kishor <ramkishor0509@gmail.com>
2019-06-12 20:56:07 +0530

    ok

--- src/main/java/org/brijframework/test/jpa/TestJPA.java
+++ src/main/java/org/brijframework/test/jpa/TestJPA.java
@@ -1,5 +1,11 @@
 package org.brijframework.test.jpa;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.brijframework.jdbc.factories.SchemaFactory;
+import org.brijframework.model.context.ModelContext;
+
 public class TestJPA {
 	public static void main(String[] args) {
 		/*
@@ -9,5 +15,14 @@ public class TestJPA {
 		 * context.setProperty(EntityConstants.IMPORT_ADPTER_CLASS,
 		 * EntityProcessorImpl.class.getName()); context.start();
 		 */
+		ModelContext context=new ModelContext();
+		context.startup();
+		Connection dataSource;
+		try {
+			dataSource = SchemaFactory.factory().getConnection("DEV_DATA_SOURCE");
+			System.out.println(dataSource);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
