d6ccc7f1d7bb1719c362fd6dd85e9b57e8c98e94
workmy2018 <workmy2018@gmail.com>
2019-06-13 10:21:38 +0530

    ok

--- src/main/java/org/brijframework/test/jpa/TestJPA.java
+++ src/main/java/org/brijframework/test/jpa/TestJPA.java
@@ -1,6 +1,8 @@
 package org.brijframework.test.jpa;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.brijframework.jdbc.factories.SchemaFactory;
@@ -15,14 +17,21 @@ public class TestJPA {
 		 * context.setProperty(EntityConstants.IMPORT_ADPTER_CLASS,
 		 * EntityProcessorImpl.class.getName()); context.start();
 		 */
-		ModelContext context=new ModelContext();
+		ModelContext context = new ModelContext();
 		context.startup();
-		Connection dataSource;
+
+		Connection jdbcConnection;
 		try {
-			dataSource = SchemaFactory.factory().getConnection("DEV_DATA_SOURCE");
-			System.out.println(dataSource);
+			jdbcConnection = SchemaFactory.factory().getConnection("DEV_DATA_SOURCE");
+			DatabaseMetaData metaData=jdbcConnection.getMetaData();
+			ResultSet tables = metaData.getTables(null, null, null, 
+			         new String[] {"TABLE"});
+			while (tables.next()) {
+				System.out.println(tables.getRow()+"table = " + tables.getString("TABLE_NAME"));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
