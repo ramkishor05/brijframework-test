8051f9055ce9be609810673e2e2734bc4a48b60a
ramkishor05 <ramkishor05@gmail.com>
2019-06-14 11:02:26 +0530

    ramkishor05

--- src/main/java/org/brijframework/test/jpa/TestJPA.java
+++ src/main/java/org/brijframework/test/jpa/TestJPA.java
@@ -1,14 +1,9 @@
 package org.brijframework.test.jpa;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import org.brijframework.jdbc.constants.JdbcMeta;
-import org.brijframework.jdbc.context.JdbcContext;
-import org.brijframework.jdbc.factories.asm.DataSourceJdbcFactory;
-import org.brijframework.jdbc.util.JdbcUtil;
-import org.brijframework.model.context.ModelContext;
+import org.brijframework.asm.context.ApplicationContext;
 import org.brijframework.model.env.PropEnvironment;
+import org.brijframework.resources.asm.container.FileResourceContainer;
+import org.brijframework.resources.container.ResourceContainer;
 
 public class TestJPA {
 	public static void main(String[] args) {
@@ -19,22 +14,25 @@ public class TestJPA {
 		 * context.setProperty(EntityConstants.IMPORT_ADPTER_CLASS,
 		 * EntityProcessorImpl.class.getName()); context.start();
 		 */
-		PropEnvironment environment=new PropEnvironment();
-		
-		
-		
-		ModelContext context = new ModelContext();
+		ApplicationContext context=new  ApplicationContext();
 		context.startup();
 		
-		JdbcContext jdbcContext=new JdbcContext();
-		
-		Connection jdbcConnection;
-		try {
-			jdbcConnection = DataSourceJdbcFactory.factory().getConnection("DEV_DATA_SOURCE");
-			System.out.println(JdbcUtil.getTablesList(jdbcConnection,JdbcMeta.TABLE.toString()));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		/*
+		 * PropEnvironment environment=new PropEnvironment();
+		 * System.out.println(environment.getProperties());
+		 */
+		/*
+		 * ModelContext context = new ModelContext();
+		 * 
+		 * context.startup();
+		 * 
+		 * JdbcContext jdbcContext=new JdbcContext();
+		 * 
+		 * Connection jdbcConnection; try { jdbcConnection =
+		 * DataSourceJdbcFactory.factory().getConnection("DEV_DATA_SOURCE");
+		 * System.out.println(JdbcUtil.getTablesList(jdbcConnection,JdbcMeta.TABLE.
+		 * toString())); } catch (SQLException e) { e.printStackTrace(); }
+		 */
 
 	}
 }
