1363f195cc8b2b423fe308dc20c1b9bf92377fa5
ram kishor <ramkishor0509@gmail.com>
2019-06-27 20:46:50 +0530

    ok

--- src/main/java/org/brijframework/boot/AppBoot.java
+++ src/main/java/org/brijframework/boot/AppBoot.java
@@ -1,11 +1,18 @@
 package org.brijframework.boot;
 
+import java.io.File;
+import java.util.Properties;
+
 import org.brijframework.context.ApplicationContext;
+import org.brijframework.util.resouces.YamlUtil;
 
 public class AppBoot {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ApplicationContext();
-		context.startup();
+		/*ApplicationContext context = new ApplicationContext();
+		context.startup();*/
+		Properties envproperties=YamlUtil.getEnvProperties(new File("E:\\brijframe\\brijframework-test\\src\\main\\resources\\application.yml"));
+		//System.out.println(envproperties.get("application.bootstrap.config.model.data"));
+		System.out.println(envproperties);
 	}
 }
