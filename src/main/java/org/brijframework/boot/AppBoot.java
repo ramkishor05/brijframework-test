241869428f998041a1548fd9d22b7cc7e600e4d8
ram kishor <ramkishor0509@gmail.com>
2019-11-18 20:55:32 +0530

    ok

--- src/main/java/org/brijframework/boot/AppBoot.java
+++ src/main/java/org/brijframework/boot/AppBoot.java
@@ -1,14 +1,14 @@
 package org.brijframework.boot;
 
-import org.brijframework.context.ApplicationContext;
-import org.brijframework.context.integration.ApplicationContextFactory;
-import org.brijframework.model.context.ModelContext;
+import org.brijframework.bean.context.BeanContext;
+import org.brijframework.bean.context.factories.BeanContextFactory;
 
 public class AppBoot {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContextFactory applicationBoot=ApplicationContextFactory.getFactory();
-		ApplicationContext applicationContext = applicationBoot.getApplicationContext();
-		System.out.println(applicationContext.getContexts(ModelContext.class));
+		//ApplicationContextFactory applicationBoot=ApplicationContextFactory.getFactory();
+		//ApplicationContext applicationContext = applicationBoot.getApplicationContext();
+		BeanContext beanContext = BeanContextFactory.getFactory().getBeanContext(BeanContext.class);
+		System.out.println("beanContext="+beanContext);
 	}
 }
