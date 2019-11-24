abc1f8c3c14f671f0437bbec94c4742f495a4ff0
phpoms <phpoms>
2019-11-25 02:29:30 +0530

    ok

--- src/main/java/org/brijframework/boot/ApplicationContextFactoryTest.java
+++ src/main/java/org/brijframework/boot/ApplicationContextFactoryTest.java
@@ -2,15 +2,17 @@ package org.brijframework.boot;
 
 import org.brijframework.context.ApplicationContext;
 import org.brijframework.context.factories.ApplicationContextFactory;
-import org.brijframework.support.config.application.ApplicationConfigration;
-import org.brijframework.support.config.application.DatasourceConfigration;
+import org.brijframework.context.factories.ApplicationEnvironmentFactory;
+import org.brijframework.env.Environment;
+import org.brijframework.support.config.application.EnvironmentResource;
 
-@ApplicationConfigration("")
-@DatasourceConfigration(files = "")
+@EnvironmentResource
 public class ApplicationContextFactoryTest {
 
 	public static void main(String[] args) throws Exception {
 		ApplicationContext beanContext = ApplicationContextFactory.getFactory().getApplicationContext();
 		System.out.println("beanContext="+beanContext);
+		Environment environment = ApplicationEnvironmentFactory.getFactory().getEnvironment();
+		System.out.println("environment="+environment);
 	}
 }
